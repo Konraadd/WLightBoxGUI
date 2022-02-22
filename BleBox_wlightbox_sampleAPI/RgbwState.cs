efc59f650c6425bd4d9529e767b18c2f9c043aac
Konrad Kłosowicz <konraaddk@gmail.com>
2022-02-22 01:15:57 +0100

    Added API service.

---
+++ BleBox_wlightbox_sampleAPI/RgbwState.cs
@@ -0,0 +1,23 @@
+﻿namespace BleBox_wlightbox_sampleAPI;
+public class DurationsMs
+{
+    public int colorFade { get; set; }
+    public int effectFade { get; set; }
+    public int effectStep { get; set; }
+}
+
+public class Rgbw
+{
+    public int colorMode { get; set; }
+    public int effectID { get; set; }
+    public string desiredColor { get; set; }
+    public string currentColor { get; set; }
+    public string lastOnColor { get; set; }
+    public DurationsMs durationsMs { get; set; }
+}
+
+public class RgbwState
+{
+    public Rgbw rgbw { get; set; }
+}
+
---
+++ BleBox_wlightbox_sampleAPI/WLightBoxService.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Http.Json;
+
+
+namespace BleBox_wlightbox_sampleAPI
+{
+
+    public class WLightBoxService
+    {
+        private readonly string _endpoint;
+        private readonly HttpClient _client;
+
+        public WLightBoxService(string endpoint)
+        {
+            _endpoint = endpoint;
+            _client = new HttpClient();
+        }
+        public Task SetColor(string desiredColor, double colorFade = 1000)
+        {
+            var content = new
+            {
+                rgbw = new
+                {
+                    desiredColor,
+                    durationsMs = new
+                    {
+                        colorFade
+                    }
+                }
+            };
+
+            return _client.PostAsJsonAsync(_endpoint + "/api/rgbw/set", content);
+        }
+
+        public Task SetEffect(int effectID, int effectFade = 1000, int effectStep = 2000)
+        {
+            var content = new
+            {
+                rgbw = new
+                {
+                    effectID,
+                    durationsMs = new
+                    {
+                        effectFade,
+                        effectStep
+                    }
+                }
+            };
+
+            return _client.PostAsJsonAsync(_endpoint + "/api/rgbw/set", content);
+        }
+
+        public async Task<string> GetColor()
+        {
+            var result = await _client.GetAsync(_endpoint + "/api/rgbw/state");
+            var currentState = result.Content.ReadFromJsonAsync<RgbwState>().Result;
+            return currentState.rgbw.currentColor;
+        }
+
+        public async Task<int> GetEffect()
+        {
+            var result = await _client.GetAsync(_endpoint + "/api/rgbw/state");
+            var currentState = result.Content.ReadFromJsonAsync<RgbwState>().Result;
+            return currentState.rgbw.effectID;
+        }
+    }
+}
